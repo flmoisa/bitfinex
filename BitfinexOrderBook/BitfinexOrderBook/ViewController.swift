26d9ac42c6b3c76a83481fde7a16c2614e35d80e
Florin Moisa <flmoisa@gmail.com>
2020-09-24 09:46:15 +0300

    websocket library - starscream

--- BitfinexOrderBook/BitfinexOrderBook/ViewController.swift
+++ BitfinexOrderBook/BitfinexOrderBook/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import Starscream
 
 class ViewController: UIViewController {
-
+    
+    private var socket: WebSocket?
+    private var isConnected = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var request = URLRequest(url: URL(string: "wss://api-pub.bitfinex.com/ws/2")!)
+        request.timeoutInterval = 5
+        socket = WebSocket(request: request)
+        socket?.connect()
+        socket?.onEvent = { [unowned self] event in
+            switch event {
+            case .connected(let headers):
+                self.isConnected = true
+                print("websocket is connected: \(headers)")
+            case .disconnected(let reason, let code):
+                self.isConnected = false
+                print("websocket is disconnected: \(reason) with code: \(code)")
+            case .text(let string):
+                print("Received text: \(string)")
+            case .binary(let data):
+                print("Received data: \(data.count)")
+            case .ping(_):
+                break
+            case .pong(_):
+                break
+            case .viabilityChanged(_):
+                break
+            case .reconnectSuggested(_):
+                break
+            case .cancelled:
+                self.isConnected = false
+            case .error(let error):
+                self.isConnected = false
+                print("Received error: \(error.debugDescription)")
+            }
+        }
     }
-
-
+    
+    
+    
 }
 
---
+++ BitfinexOrderBook/Pods/Target Support Files/Pods-BitfinexOrderBookUITests/Pods-BitfinexOrderBookUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BitfinexOrderBookUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BitfinexOrderBookUITests
+@end
